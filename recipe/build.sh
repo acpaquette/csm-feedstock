448a9922b05698bfb9c228de4f8d6229c8a9592f
Amy Stamile <74275278+amystamile-usgs@users.noreply.github.com>
2026-02-12 11:09:18 -0700

    remove file check

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,10 +5,3 @@ set -e # Abort on error
 mkdir build && cd build
 cmake ${CMAKE_ARGS} -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -GNinja ..
 cmake --build . --target install
-
-if [[ "$(uname)" == "Darwin" ]]; then
-    echo "Checking all .dylib architectures in $PREFIX/lib..."
-    for lib in "$PREFIX"/lib/*.dylib; do
-        file "$lib"
-    done
-fi
